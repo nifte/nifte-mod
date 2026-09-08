614f76feab62fcc4cdd2ed8ade4e975abf6732a4
nifte <9247037+nifte@users.noreply.github.com>
2026-09-08 08:17:21 -0700

    ignore tmp files

--- .tmp-mc-src/net/minecraft/client/renderer/fog/environment/package-info.java
+++
@@ -1,4 +0,0 @@
-@NullMarked
-package net.minecraft.client.renderer.fog.environment;
-
-import org.jspecify.annotations.NullMarked;
--- .tmp-mc-src/net/minecraft/gizmos/GizmoProperties.java
+++
@@ -1,9 +0,0 @@
-package net.minecraft.gizmos;
-
-public interface GizmoProperties {
-	GizmoProperties setAlwaysOnTop();
-
-	GizmoProperties persistForMillis(int milliseconds);
-
-	GizmoProperties fadeOut();
-}
